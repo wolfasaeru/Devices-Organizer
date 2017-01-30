3da38012b0ca45dc47488da480e44c57b0865b3b
Hasael Oliveros <SSMacProHasael@MacBook-Pro-de-Hasael.local>
2017-01-29 21:58:57 -0400

    Adding Material Design

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,8 +2,10 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
+import { MaterialModule } from "@angular/material";
 
 import { AppComponent } from './app.component';
+import 'hammerjs';
 
 @NgModule({
     declarations: [
@@ -13,6 +15,7 @@ import { AppComponent } from './app.component';
         BrowserModule,
         FormsModule,
         HttpModule,
+        MaterialModule.forRoot()
     ],
     providers: [],
     bootstrap: [AppComponent]
